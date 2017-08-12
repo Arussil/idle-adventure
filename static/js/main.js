704e9001b1db4fc3228fa838d209a56b59e7226b
arussil <arussil@devuan>
2017-08-12 15:44:42 +0200

    We can now kill rats

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+	var rat_killed = 0;
 
+	window.setInterval(function() {
+	});
+
+	$('#kill-rat').on('click', function() {
+		if (rat_killed === 0) {
+			$('#killcount-rat').removeClass('hidden');
+		} else if(rat_killed === 20) {
+			$('#inventory').removeClass('hidden');
+		}
+		rat_killed++;
+		$('#killcount-rat > span').html(rat_killed);
+	})
+});
